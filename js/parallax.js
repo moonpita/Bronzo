6a30cff94726501f9c5db2f98bd1a6f8b221ce01
moonpita <75728982+moonpita@users.noreply.github.com>
2021-04-12 20:11:20 +0300

    Delete parallax.js

--- js/parallax.js
+++
@@ -1,9 +0,0 @@
-function parallax(event) {
-    this.querySelectorAll('.parallax__layer').forEach(layer => {
-        let speed = layer.getAttribute('data-speed');
-        console.log(layer.getAttribute('data-speed'));
-        layer.style.transform = `translate(${event.clientX*speed/2000}px, ${event.clientY*speed/2000}px)`
-    });
-}
-
-document.addEventListener('mousemove', parallax);
