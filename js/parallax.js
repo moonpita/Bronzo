42881ebed21a4e4f5e086e2d451244bfada3ca00
moonpita <75728982+moonpita@users.noreply.github.com>
2021-04-14 19:30:41 +0300

    Delete js directory

--- js/parallax.js
+++
@@ -1,8 +0,0 @@
-function parallax(event) {
-    this.querySelectorAll('.parallax__layer').forEach(layer => {
-        let speed = layer.getAttribute('data-speed');
-        layer.style.transform = `translate(${event.clientX*speed/800}px)`
-    });
-}
-
-document.addEventListener('mousemove', parallax);
